a732ec107af2a350579ff0b36a6c1b861977c5f0
Cool14L <45010080+Cool14L@users.noreply.github.com>
2018-11-27 18:42:57 +0100

    humanoïd

---
+++ Humanoides.java
@@ -0,0 +1,65 @@
+public class Humanoides {
+
+protected String name;
+protected int position_x;
+protected int position_y;
+protected int action;
+protected int health;
+
+	public Humanoides() { // constructor
+		this.name = NULL;
+		this.position_x = 0;
+		this.position_y = 0;
+		this.action = 1;
+		this.health = 2;
+	}
+
+	public void attack() {
+
+	}
+
+	public void walk() {
+
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getPosition_x() {
+		return position_x;
+	}
+
+	public void setPosition_x(int position_x) {
+		this.position_x = position_x;
+	}
+
+	public int getPosition_y() {
+		return position_y;
+	}
+
+	public void setPosition_y(int position_y) {
+		this.position_y = position_y;
+	}
+
+	public int getAction() {
+		return action;
+	}
+
+	public void setAction(int action) {
+		this.action = action;
+	}
+
+	public int getHealth() {
+		return health;
+	}
+
+	public void setHealth(int health) {
+		this.health = health;
+	}
+
+}
