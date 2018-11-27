57613adda3ffd613f12e46ca20dedbf4b250cf18
Cool14L <45010080+Cool14L@users.noreply.github.com>
2018-11-27 19:04:26 +0100

    case
    constructeur à définir, comment la case va agir avec le plateau

---
+++ Case.java
@@ -0,0 +1,45 @@
+public class Case {
+
+private int noise;
+private int nbStudent;
+private int nbProfessor;
+private boolean isPossibleObjective;
+private boolean isTrueObjective;
+
+public void splitting() {
+
+	}
+public Case() {
+	
+}
+public int getNoise() {
+	return noise;
+}
+public void setNoise(int noise) {
+	this.noise = noise;
+}
+public int getNbStudent() {
+	return nbStudent;
+}
+public void setNbStudent(int nbStudent) {
+	this.nbStudent = nbStudent;
+}
+public int getNbProfessor() {
+	return nbProfessor;
+}
+public void setNbProfessor(int nbProfessor) {
+	this.nbProfessor = nbProfessor;
+}
+public boolean isPossibleObjective() {
+	return isPossibleObjective;
+}
+public void setPossibleObjective(boolean isPossibleObjective) {
+	this.isPossibleObjective = isPossibleObjective;
+}
+public boolean isTrueObjective() {
+	return isTrueObjective;
+}
+public void setTrueObjective(boolean isTrueObjective) {
+	this.isTrueObjective = isTrueObjective;
+}
+}
