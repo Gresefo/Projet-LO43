07ce256c7b108e782091959d3cea43eb471c167d
Cool14L <45010080+Cool14L@users.noreply.github.com>
2018-12-10 15:10:34 +0100

    Case.java
    ajout des link pour les cases nord sud est ouest, et un numéro pour leurs organisation sur le Board

--- Case.java
+++ Case.java
@@ -1,20 +1,46 @@
 public class Case {
-
+private int num;
 private int noise;
 private int nbStudent;
 private int nbProfessor;
 private boolean isPossibleObjective;
 private boolean isTrueObjective;
+private Case nord, sud, est, ouest;
 
-public void splitting() {
-
-	}
+public Case(int i) {
+	this.num=i;
+}
 public Case() {
-	
+	num = 0;
 }
 public int getNoise() {
 	return noise;
 }
+public void setNord(Case n) {
+	this.nord = n;
+}
+public Case getNord() {
+	return nord;
+}
+public void setSud(Case s) {
+	this.sud = s;
+}
+public Case getSud() {
+	return sud;
+}
+public void setEst(Case e) {
+	this.est = e;
+}
+public Case getEst() {
+	return est;
+}
+public void setOuest(Case o) {
+	this.ouest = o;
+}
+public Case getOuest() {
+	return ouest;
+}
+
 public void setNoise(int noise) {
 	this.noise = noise;
 }
