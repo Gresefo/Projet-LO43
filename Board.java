c2e278835468d992ba1247dac075285c6dde07c5
Cool14L <45010080+Cool14L@users.noreply.github.com>
2018-12-11 15:14:22 +0100

    Board.java
    logiquement, toute les cases sont crées avec le consutructor, et le start et end sont les cases de départ et d'arrivé

---
+++ Board.java
@@ -0,0 +1,34 @@
+//import java.util.*;
+//import java.io.*;
+
+public class Board {
+	static final int SIZE = 32;
+	Case board[][];
+	//private int size;
+	private Case start, end;
+
+	public Board() {
+		
+		for (int i=0; i<= 8; i++) {
+			for (int j=0; j<=4; j++) {
+				Case c = new Case();
+				board[i][j] = c;
+				
+			}
+				
+		}
+	}
+	public Case getStart() {
+		return this.start;
+	}
+	
+	public void setStart(int x, int y) {
+		Case re = new Case();
+		for(int i=0; i<= 8 || i!=x; i++) {
+			for(int j=0; j<=4 || j !=y; j++) {
+				board[i][j] = re;
+			}
+	}
+		this.start= re;
+	}
+}
